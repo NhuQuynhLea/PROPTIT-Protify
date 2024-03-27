65e2e52992d13db33d4608bba90a04e304520db3
Lê Như Quỳnh <110320254+NhuQuynhLea@users.noreply.github.com>
2024-03-27 19:50:55 +0700

    fix-review MainUI AlbumDetail

--- app/src/main/java/com/example/proptit_protify/service/MediaPlaybackService.kt
+++ app/src/main/java/com/example/proptit_protify/service/MediaPlaybackService.kt
@@ -1,4 +1,30 @@
 package com.example.proptit_protify.service
 
-class MediaPlaybackService {
+import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
+import androidx.media.MediaBrowserServiceCompat
+
+
+
+
+class MediaPlaybackService : MediaBrowserServiceCompat() {
+
+
+
+
+    override fun onGetRoot(
+        clientPackageName: String,
+        clientUid: Int,
+        rootHints: Bundle?
+    ): BrowserRoot? {
+        return null
+    }
+
+    override fun onLoadChildren(
+        parentId: String,
+        result: Result<MutableList<MediaBrowserCompat.MediaItem>>
+    ) {
+
+    }
+
 }
