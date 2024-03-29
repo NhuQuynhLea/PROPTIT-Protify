f8e84e9fe49d937c17c8b37f29d8666953202b41
Lê Như Quỳnh <110320254+NhuQuynhLea@users.noreply.github.com>
2024-03-30 00:34:30 +0700

    feat-fix MainUI and mock data to test

--- app/src/main/java/com/example/proptit_protify/models/Song.kt
+++ app/src/main/java/com/example/proptit_protify/models/Song.kt
@@ -1,4 +1,10 @@
 package com.example.proptit_protify.models
 
-class Song {
-}
+import java.io.Serializable
+
+data class Song (
+  val title:String,
+  val artist:String,
+  val image: String,
+  val resource: String
+): Serializable
