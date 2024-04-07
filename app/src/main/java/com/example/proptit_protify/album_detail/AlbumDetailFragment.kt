0b469e45733e4a7e5d734390f1fd30c7ed1c9c6f
Lê Như Quỳnh <110320254+NhuQuynhLea@users.noreply.github.com>
2024-04-07 23:01:12 +0700

    fix-refactor AlbumDetailAdapter

--- app/src/main/java/com/example/proptit_protify/album_detail/AlbumDetailFragment.kt
+++ app/src/main/java/com/example/proptit_protify/album_detail/AlbumDetailFragment.kt
@@ -6,17 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.proptit_protify.databinding.FragmentAlbumDetailBinding
+import com.example.proptit_protify.interfaces.ItemClick
 import com.example.proptit_protify.models.Song
 
 
-class AlbumDetailFragment : Fragment() {
+class AlbumDetailFragment : Fragment(), ItemClick {
     private lateinit var binding: FragmentAlbumDetailBinding
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: AlbumDetailAdapter
-    private lateinit var listData: List<Song>
+    private lateinit var viewModel: AlbumDetailViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,31 +27,35 @@ class AlbumDetailFragment : Fragment() {
     ): View {
         binding = FragmentAlbumDetailBinding.inflate(inflater,container,false)
         initComponent()
-        getList()
+
+
 
         return binding.root
     }
 
     private fun initComponent() {
-        listData = ArrayList()
         recyclerView = binding.recyclerview
-        adapter = AlbumDetailAdapter(requireContext(), listData)
+        adapter = AlbumDetailAdapter(requireContext(), this)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(context)
-    }
-
-    private fun getList(){
-        val list: List<Song> = listOf(
-            Song("Title1", "Artist1", "Album1", "Genre1"),
-            Song("Title2", "Artist2", "Album2", "Genre2"),
-            Song("Title3", "Artist3", "Album3", "Genre3"),
-            Song("Title4", "Artist4", "Album4", "Genre4")
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                context,
+                DividerItemDecoration.VERTICAL
+            )
         )
-        adapter.setData(list)
-        listData = list
+
+        viewModel = ViewModelProvider(this)[AlbumDetailViewModel::class.java]
+        viewModel.getList()
+        viewModel.listSongs.observe(viewLifecycleOwner) {
+            adapter.setData(it)
+        }
     }
 
 
+    override fun onCLick(song: Song) {
+
+    }
 
 
 }
